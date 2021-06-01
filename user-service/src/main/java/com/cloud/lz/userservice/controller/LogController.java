c88753eac4db3a3c7fbb4c44781d44e08dbc1949
liuze <564820771>
2021-06-01 11:26:23 +0800

    fetch

--- user-service/src/main/java/com/cloud/lz/userservice/controller/LogController.java
+++ user-service/src/main/java/com/cloud/lz/userservice/controller/LogController.java
@@ -1,10 +1,16 @@
 package com.cloud.lz.userservice.controller;
 
 import com.cloud.lz.userservice.service.LogService;
+import org.apache.tomcat.util.security.MD5Encoder;
+import org.bouncycastle.crypto.digests.MD5Digest;
+import org.hibernate.loader.plan.spi.QuerySpaceUidNotRegisteredException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.security.rsa.RSASignature;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/log")
@@ -21,4 +27,10 @@ public class LogController {
         return null;
     }
 
+    public static void main(String[] args) {
+        String s = UUID.nameUUIDFromBytes("liuze0118".getBytes()).toString();
+        MD5Digest md5Digest = new MD5Digest();
+        System.out.println("s="+s);
+        System.out.println("encode="+1);
+    }
 }
